a55bca95daf384ee7bdebadd732bd41fb956491a
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-02 15:00:22 +0300

    feat: map Google OAuth2 credentials from environment to system properties

--- src/main/java/com/sahnesen/api/sahnesen/config/ConfigLoader.java
+++ src/main/java/com/sahnesen/api/sahnesen/config/ConfigLoader.java
@@ -8,5 +8,8 @@ public class ConfigLoader {
         System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("GOOGLE_REDIRECT_URI", dotenv.get("GOOGLE_REDIRECT_URI"));
     }
 }
