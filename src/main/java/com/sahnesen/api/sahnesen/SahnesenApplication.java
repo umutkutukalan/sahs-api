6bcf1004d0cecb013a7d5dece0702f16e7a08e1e
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-02 14:46:01 +0300

    chore: initial commit with spring boot structure

---
+++ src/main/java/com/sahnesen/api/sahnesen/SahnesenApplication.java
@@ -0,0 +1,13 @@
+package com.sahnesen.api.sahnesen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahnesenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahnesenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sahnesen/api/sahnesen/SahnesenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahnesen.api.sahnesen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahnesenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
