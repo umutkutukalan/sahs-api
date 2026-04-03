85d6a2129e66b37163879d3b77feee97e1502b96
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-03 16:35:45 +0300

    test: setup dedicated test profile and configuration

--- src/test/java/com/sahnesen/api/sahnesen/SahnesenApplicationTests.java
+++ src/test/java/com/sahnesen/api/sahnesen/SahnesenApplicationTests.java
@@ -2,8 +2,10 @@ package com.sahnesen.api.sahnesen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SahnesenApplicationTests {
 
 	@Test
