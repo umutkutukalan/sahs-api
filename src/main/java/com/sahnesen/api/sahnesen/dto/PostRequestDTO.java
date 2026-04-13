e5351e851b6a34aeeefac886cbef1c228e799edb
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-13 13:53:00 +0300

    feat(post): add PostType enum and integrate into PostRequestDTO and Post entity

--- src/main/java/com/sahnesen/api/sahnesen/dto/PostRequestDTO.java
+++ src/main/java/com/sahnesen/api/sahnesen/dto/PostRequestDTO.java
@@ -1,11 +1,17 @@
 package com.sahnesen.api.sahnesen.dto;
 
+import com.sahnesen.api.sahnesen.enums.PostType;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PostRequestDTO {
+
+    @NotNull(message = "Lütfen içeriğiniz için bir tür seçiniz.")
+    private PostType postType; // Yazı türü (BLOG, STUDY, PROJECT, TUTORIAL, OPINION)
 
     @NotBlank(message = "Başlık boş olamaz.")
     @Size(min = 3, max = 150, message = "Başlık 3 ile 150 karakter arasında olmalıdır.")
--- src/main/java/com/sahnesen/api/sahnesen/entities/Post.java
+++ src/main/java/com/sahnesen/api/sahnesen/entities/Post.java
@@ -5,8 +5,12 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.sahnesen.api.sahnesen.enums.PostType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +36,10 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PostType postType;
 
     @Column(nullable = false)
     private String title;
