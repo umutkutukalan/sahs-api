665ff385b6e1dd3d3b097b64110f33fcc46b62d7
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-16 14:51:03 +0300

    feat(dependencies): add spring-boot-starter-data-redis dependency

--- src/main/java/com/sahnesen/api/sahnesen/SahnesenApplication.java
+++ src/main/java/com/sahnesen/api/sahnesen/SahnesenApplication.java
@@ -2,9 +2,11 @@ package com.sahnesen.api.sahnesen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import com.sahnesen.api.sahnesen.config.ConfigLoader;
 
+@EnableCaching // Önbellekleme mekanizmasını etkinleştirir
 @SpringBootApplication
 public class SahnesenApplication {
 
