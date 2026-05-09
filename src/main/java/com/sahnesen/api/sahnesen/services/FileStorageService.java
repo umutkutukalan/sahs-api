7c28524f1051382723eca2dd4aeec549068ffd43
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-05-09 20:38:56 +0300

    feat(FileStorageService): implement file storage service for handling uploads

---
+++ src/main/java/com/sahnesen/api/sahnesen/services/FileStorageService.java
@@ -0,0 +1,39 @@
+package com.sahnesen.api.sahnesen.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    public FileStorageService(@Value("${file.upload-dir}") String uploadDir) {
+        this.fileStorageLocation = Paths.get(uploadDir).toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new RuntimeException("Klasör oluşturulamadı.", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = UUID.randomUUID().toString() + "_" + StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName; // DB'ye sadece bu ismi kaydedeceğiz
+        } catch (Exception e) {
+            throw new RuntimeException("Dosya kaydedilemedi.", e);
+        }
+    }
+
+}
