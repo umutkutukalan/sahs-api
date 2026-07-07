21784d483f7bf4d4e34e4e01bbd3876f97e454cc
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-07-07 11:22:39 +0300

    feat: add docker-compose support, upgrade Flyway configuration, and update database property naming conventions

--- src/main/java/com/sahnesen/api/sahnesen/config/ConfigLoader.java
+++ src/main/java/com/sahnesen/api/sahnesen/config/ConfigLoader.java
@@ -6,7 +6,7 @@ public class ConfigLoader {
     public static void load() {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
         System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
--- src/main/resources/db/migration/V1__update_post_types.sql
+++
@@ -1,6 +0,0 @@
-UPDATE
-    posts
-SET
-    post_type = 'BLOG'
-WHERE
-    post_type IN ('STUDY', 'TUTORIAL', 'OPINION');
